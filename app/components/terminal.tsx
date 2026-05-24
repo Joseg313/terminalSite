6c929a0e99134630824ede76246500a3dc57a01a
Jose <501jag3@gmail.com>
2026-05-23 22:17:53 -0500

    experimenting with reading lines

--- app/components/terminal.tsx
+++ app/components/terminal.tsx
@@ -6,6 +6,9 @@ import 'xterm/css/xterm.css';
 const TerminalComponent = () => {
   const terminalRef = useRef<HTMLDivElement>(null);
   
+  
+
+
   useEffect (() => {
     if (!terminalRef.current) return;
     const term = new Terminal();
@@ -14,13 +17,40 @@ const TerminalComponent = () => {
     term.writeln('Welcome to portfolio')
     
     term.onData((data) => term.write(data));
+    
+    term.onKey(ev => {
+      if (ev.domEvent.key === "Enter") { 
+        
+        //select current line
+        console.log(term.rows)
+        const currentLineIndex = 1
+        term.selectLines(currentLineIndex, currentLineIndex)
+        const currentLine = term.getSelection()
+        term.writeln(currentLine)
+        // if (currentLine.trim().length == 0) {
+        //   term.writeln("")
+        //   term.write("$  ")
+        // } else if (currentLine === "clear") {
+        //   term.clear()
+        // }
+        
+        term.clearSelection()
+        
+        
+        // if empty just writeln("")
+
+        // if something on line, read and store it and try to match it with something
+      
+      }
+    })
+    
 
     return () => {
       term.dispose();
     };
   }, [])
   return <div ref={terminalRef}></div>;
-};
+}; 
 
 export default TerminalComponent;
 
