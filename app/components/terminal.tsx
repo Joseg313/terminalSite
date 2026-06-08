2ea4886d24bf80cd55e4c6fa67d78632368dbe89
Jose <501jag3@gmail.com>
2026-06-08 18:56:24 -0500

    much animation progress there is current bug with big name getting deleted

--- app/components/terminal.tsx
+++ app/components/terminal.tsx
@@ -7,8 +7,103 @@ const TerminalComponent = () => {
   const terminalRef = useRef<HTMLDivElement>(null);
   const delay = (ms:number) => new Promise(resolve => setTimeout(resolve, ms));
   const symbs = ["%","&","*","#","[","^","@","%",")","X"]
-
-
+  const subTitle = "thinker, programmer, learner"
+  // hold the current indicies for where the animation is at
+  let colAnimationIndicies = {
+    col67: 25,
+    col68: 25,
+    col69: 25,
+    col70: 25,
+    col71: 25,
+    col72: 25,
+    col73: 25,
+    col74: 25,
+    col75: 25,
+    col76: 25,
+    col77: 25,
+    col78: 25,
+    col79: 25,
+    col80: 25,
+    col81: 25,
+    col82: 25,
+    col83: 25,
+    col84: 25,
+    col85: 25,
+    col86: 25,
+    col87: 25,
+    col88: 25,
+    col89: 25,
+    col90: 25,
+    col91: 25,
+    col92: 25,
+    col93: 25,
+    col94: 25,
+    col95: 25,
+    col96: 25,
+    col97: 25,
+    col98: 25,
+    col99: 25,
+    col100: 25,
+    col101: 25,
+    col102: 25,
+    col103: 25,
+    col104: 25,
+    col105: 25,
+    col106: 25,
+    col107: 25,
+    col108: 25,
+    col109: 25,
+    col110: 25,
+    col111: 25,
+    col112: 25,
+    col113: 25,
+    col114: 25,
+    col115: 25,
+    col116: 25,
+    col117: 25,
+    col118: 25,
+    col119: 25,
+    col120: 25,
+    col121: 25,
+    col122: 25,
+    col123: 25,
+    col124: 25,
+    col125: 25,
+    col126: 25,
+    col127: 25,
+    col128: 25,
+    col129: 25,
+    col130: 25,
+    col131: 25,
+    col132: 25,
+    col133: 25,
+    col134: 25,
+    col135: 25,
+    col136: 25,
+    col137: 25,
+    col138: 25,
+    col139: 25,
+    col140: 25,
+    col141: 25,
+    col142: 25,
+    col143: 25,
+    col144: 25,
+    col145: 25,
+    col146: 25,
+    col147: 25,
+    col148: 25,
+    col149: 25,
+    col150: 25,
+    col151: 25,
+    col152: 25,
+    col153: 25,
+    col154: 25,
+    col155: 25,
+    col156: 25,
+    col157: 25,
+    col158: 25,
+    col159: 25
+  } 
   useEffect (() => {
     if (!terminalRef.current) return;
     const term = new Terminal({
@@ -22,14 +117,35 @@ const TerminalComponent = () => {
  
     term.loadAddon(new WebLinksAddon());
     
-    // fetch('/ascii-art2.txt')
-    fetch('/name.txt')  
+
+    async function titleType() {
+      
+      term.write("\x1b[102G\x1b[0m")
+      term.write("\x1b[16A\x1b[0m")
+      
+      for (const character of subTitle) {
+        term.write(character)
+        // TODO turn this back to 100
+        await delay(1)
+      }
+      term.write("\x1b8")
+    }
+    
+
+    
+    fetch('/title.txt')  
       .then((response) => response.text())
       .then((text) => {
         text.split('\n').forEach((line) => term.writeln(line))
         term.write("$ ")
-      })
-
+        term.write("\x1b7")
+        titleType()
+        
+    })
+    
+    
+    
+    
     let input_buffer = ""
     
     
@@ -78,57 +194,51 @@ const TerminalComponent = () => {
           // set cursor to start postition
           term.write("\x1b[?25l")
           
-          term.write("\x1b[65G \x1b[0m")
-          term.write("\x1b[19A \x1b[0m")
+        
+          term.write("\x1b[25;67H\x1b[0m")
           
-          // oneLetter writes a single raindrop column
-          async function oneColumn(characters:Array<string>) {
+          
+          
+          async function oneColumn(symbs:Array<string>) {
             while (input_buffer.length === 0) {
-              for (let i = 0; i<15; i++){
-                
+              const currentColIndex:number = (Math.floor(Math.random() * (159 - 67 + 1)) + 67)
+              // const currentColIndex:number = 113
+              const currentCol:string = "col"+currentColIndex.toString()
+              const currentRowIndex:number = colAnimationIndicies[currentCol]
+              // TODO go back and delete the characters in the line
+              if (currentRowIndex > 40) {
+                for (let i = 0; i<15; i++){
+                  term.write(" ")
+                  // move 1 left
+                  term.write("\x1b[1D\x1b[0m")
+                  // move 1 up
+                  term.write("\x1b[1A\x1b[0m")
+                }
+                colAnimationIndicies[currentCol] = 25
+              } else {
+                // move cursor to position described above
+                term.write(`\x1b[${currentRowIndex};${currentColIndex}H\x1b[0m`)
                 term.write(symbs[Math.floor(Math.random() * symbs.length)])
-                await delay(200)
-                // move 1 left
-                term.write("\x1b[1D\x1b[0m")
-                term.write(symbs[Math.floor(Math.random() * symbs.length)])
-
+                // term.write("X")
                 // move 1 left
                 term.write("\x1b[1D\x1b[0m")
                 // move 1 down
                 term.write("\x1b[1B\x1b[0m")
 
-              }
-              term.write("\x1b[15A\x1b[0m")
-              for (let i = 0; i<15; i++){
-                
-                term.write(" ")
-                
-          
-
-                // move 1 left
-                term.write("\x1b[1D\x1b[0m")
-                // move 1 down
-                term.write("\x1b[1B\x1b[0m")
+                // update new cursor position
+                colAnimationIndicies[currentCol]++
+                // console.log(colAnimationIndicies[currentCol])
+                await delay(20)
 
               }
-              term.write("\x1b[15A\x1b[0m")
+              
             }
             
           }
           
           oneColumn(symbs)
           
-          // TODO call this function below for each character in symbs and pass in the symb on each iteration
           
-          // async function lineOfLetters() {
-          //   for (const symb of symbs) {
-          //     oneLetter(symb)
-          //     await delay(1000);
-          //   }
-          // }
-          // lineOfLetters()
-
-          // term.write("\x1b[?25h \x1b[0m")
         } else if (currentLine?.trim().length === 0) {
           term.writeln("")
           term.write("$ ")
