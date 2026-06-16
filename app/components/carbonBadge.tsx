21623fd952b890dedba490b5670d85174a167e99
Jose <501jag3@gmail.com>
2026-06-15 19:11:39 -0500

    built docker image

--- app/components/carbonBadge.tsx
+++
@@ -1,8 +0,0 @@
-import { WebsiteCarbonBadge } from 'react-websitecarbon-badge';
-
-class carbonBadge extends React.Component {
-  render() {
-    return <WebsiteCarbonBadge co2="0.12" percentage='89' />;
-  }
-}
-
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+// next.config.ts
+module.exports = {
+  output: "standalone",
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
